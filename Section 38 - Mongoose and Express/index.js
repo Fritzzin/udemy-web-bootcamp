6e6bb242e473d8552473d7475769e40973b7c709
Augusto Fritz <augustofritzz@gmail.com>
2025-10-03 18:32:06 -0300

    Finishing Section 38

--- Section 38 - Mongoose and Express/index.js
+++ Section 38 - Mongoose and Express/index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const path = require("path");
 const mongoose = require("mongoose");
 const Product = require("./models/product");
+const methodOverride = require("method-override");
 
 mongoose
   .connect("mongodb://localhost:27017/farmStand")
@@ -17,12 +18,67 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  console.log(products);
-  res.render("products/index", { products });
-  // res.send("ALL PRODUCTS WILL BE HERE");
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category: category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: 'All' });
+  }
+});
+
+const categories = ["fruit", "vegetable", "dairy"];
+app.get("/products/new", (req, res) => {
+  res.render("products/new", { categories });
+});
+
+app.post("/products", async (req, res) => {
+  console.log(req.body);
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  // console.log(newProduct);
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+
+  res.render("products/details", { product });
+
+  // if (product) {
+  //   res.render("products/details", { product });
+  // } else {
+  //   res.send("Error. Couldn't find a product with this ID");
+  // }
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidator: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+  // console.log(req.body);
+  // res.send("PUT REQUEST");
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect("/products");
 });
 
 app.listen(PORT, () => {
