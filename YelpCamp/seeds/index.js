0f232c452c13348d52967ba14e85eb162c3d1ebe
Augusto Fritz <augustofritzz@gmail.com>
2025-10-08 18:20:01 -0300

    Basic styling and bootstrap 5 added to Yelpcamp project

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -17,14 +17,19 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20)+10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum unde vel, quae fugit officia reprehenderit est dicta cumque deserunt. Totam, facere ab neque aliquam veniam a ex perferendis et voluptatem.",
+      price: price,
     });
     await camp.save();
   }
 };
 
-seedDB().then(()=> {
-    mongoose.connection.close();
+seedDB().then(() => {
+  mongoose.connection.close();
 });
