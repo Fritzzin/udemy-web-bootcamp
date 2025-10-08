f9f2303d460711f5452a044e5fee54f8d6f8496c
Augusto Fritz <augustofritzz@gmail.com>
2025-10-08 16:54:10 -0300

    Finishing up Section 40

--- Section 40 - Middleware/index.js
+++ Section 40 - Middleware/index.js
@@ -1,17 +1,57 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-app.use(morgan('tiny'));
+// app.use(morgan("common"));
 
-app.get('/', (req,res) => {
-    res.send("HOME PAGE!");
-})
+app.use((req, res, next) => {
+  req.requestTime = Date.now();
+  console.log(req.method, req.path);
+  next();
+});
 
-app.get('/dogs', (req,res) => {
-    res.send("woof woof");
-})
+app.use("/dogs", (req, res, next) => {
+  console.log("I love dogs!");
+  next();
+});
+
+const verifyPassword = (req, res, next) => {
+  const { password } = req.query;
+  if (password === "chickennugget") {
+    next();
+  }
+  res.send("Sorry, you need a password!");
+};
+
+// app.use((req, res, next) => {
+//   console.log("First middleware");
+//   return next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Second middleware");
+//   return next();
+// });
+
+app.get("/", (req, res) => {
+  res.send("HOME PAGE!");
+});
+
+app.get("/dogs", (req, res) => {
+  console.log(`REQUEST DATE: ${req.requestTime}`);
+  res.send("woof woof");
+});
+
+app.get("/secret", verifyPassword, (req, res) => {
+  res.send(
+    "My secret is: Sometimes I wear headphones in public so I don't have to talk to anyone."
+  );
+});
+
+app.use((req, res) => {
+  res.status(404).send("NOT FOUND!");
+});
 
 app.listen(3000, () => {
-    console.log('App is running on port 3000');
-})
+  console.log("App is running on port 3000");
+});
